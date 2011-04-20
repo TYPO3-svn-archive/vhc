1bdbbc4b85024b3bcc3ac7ddcf3b6148e89f4c00
danielregelein <danielregelein@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-20 19:36:58 +0000

    Uploadede version 0.3.0 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/vhc/trunk@46825 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "vhc".
 #
-# Auto generated 14-10-2010 22:33
+# Auto generated 20-04-2011 21:46
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'DIEHL Informatik GmbH',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '0.2.0',
-	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"b4e6";s:42:"Classes/ViewHelpers/FileiconViewHelper.php";s:4:"bb9b";s:45:"Classes/ViewHelpers/PagebrowserViewHelper.php";s:4:"0ff6";s:44:"Classes/ViewHelpers/Format/AgeViewHelper.php";s:4:"210f";s:49:"Classes/ViewHelpers/Format/FilesizeViewHelper.php";s:4:"97c0";s:57:"Classes/ViewHelpers/Format/HtmlSpecialCharsViewHelper.php";s:4:"adb4";s:50:"Classes/ViewHelpers/Format/StripTagsViewHelper.php";s:4:"e616";s:54:"Classes/ViewHelpers/Link/TelephoneNumberViewHelper.php";s:4:"8b1c";s:39:"Documentation/API/OpenOffice/en/API.sxw";s:4:"58ff";}',
+	'version' => '0.3.0',
+	'_md5_values_when_last_written' => 'a:12:{s:12:"ext_icon.gif";s:4:"b4e6";s:41:"Classes/ViewHelpers/CommentViewHelper.php";s:4:"25f4";s:39:"Classes/ViewHelpers/CountViewHelper.php";s:4:"5199";s:42:"Classes/ViewHelpers/FileiconViewHelper.php";s:4:"d99c";s:45:"Classes/ViewHelpers/PagebrowserViewHelper.php";s:4:"0ff6";s:44:"Classes/ViewHelpers/Format/AgeViewHelper.php";s:4:"db5b";s:45:"Classes/ViewHelpers/Format/DateViewHelper.php";s:4:"1e87";s:49:"Classes/ViewHelpers/Format/FilesizeViewHelper.php";s:4:"1dd4";s:57:"Classes/ViewHelpers/Format/HtmlSpecialCharsViewHelper.php";s:4:"adb4";s:50:"Classes/ViewHelpers/Format/StripTagsViewHelper.php";s:4:"e616";s:54:"Classes/ViewHelpers/Link/TelephoneNumberViewHelper.php";s:4:"8b1c";s:39:"Documentation/API/OpenOffice/en/API.sxw";s:4:"be5c";}',
 	'constraints' => array(
 		'depends' => array(
 			'extbase' => '1.2.0-0.0.0',
